70c76c27a77cb630617cb53fab6b87d79cf56486
erico07181 <erico@salesforce.com>
2022-07-07 15:25:55 -0700

    step 6

--- vaccine-hub-api/models/user.js
+++ vaccine-hub-api/models/user.js
@@ -22,5 +22,55 @@ class User {
     //Get users email and check if there is no same email in db
     //Ask for a password
     //Hash password
+    const requiredFields = ["email", "password"];
+    requiredFields.forEach((field) => {
+      if (!credentials.hasOwnProperty(field)) {
+        throw new UnauthorizedError(`Missin ${field} in request body`);
+      }
+    });
+
+    const existingUser = await User.fetchUserByEmail(credentials.email);
+    if (existingUser) {
+      throw new UnauthorizedError(`User already exists: ${credentials.email}`);
+    }
+
+    const lowercasedEmail = credentials.email.toLowerCase();
+
+    const result = await db.query(
+      `
+    INSERT INTO users(
+        first_name,
+        last_name,
+        email,
+        location,
+        password
+    )
+    VALUES ($1,$2,$3,$4,$5)
+    RETURNING id,first_name,last_name,email,location,date;
+    `,
+      [
+        credentials.first_name,
+        credentials.last_name,
+        lowercasedEmail,
+        credentials.location,
+        hashedPassword,
+      ]
+    );
+    const user = result.rows[0];
+    return user;
+  }
+
+  static async fetchUserByEmail(email) {
+    if (!email) {
+      throw new UnauthorizedError("No email provided");
+    }
+
+    const query = `SELECT * FROM users WHERE email = $1`;
+
+    const result = await db.query(query, [email.toLowerCase()]);
+
+    const user = result.rows[0];
+
+    return user;
   }
 }
--- vaccine-hub-api/routes/auth.js
+++ vaccine-hub-api/routes/auth.js
@@ -3,8 +3,22 @@ const User = require("../models/user");
 const { NotFoundError } = require("../utils/errors");
 const router = express.Router();
 
-router.post("/login", async (req, res, next) => {});
+router.post("/login", async (req, res, next) => {
+  try {
+    const user = await User.login(req.body);
+    return res.status(200).json({ user });
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.post("/register", async (req, res, next) => {});
+router.post("/register", async (req, res, next) => {
+  try {
+    const user = await User.register(req.body);
+    return res.status(201).json({ user });
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
